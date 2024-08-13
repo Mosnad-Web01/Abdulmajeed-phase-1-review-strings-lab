e386e760bd9b75454053a1d08aab9be5d9bec9e9
Abdulmajeed Alselmi <hh738hhh@gmail.com>
2024-08-13 12:23:06 +0300

    solved

---
+++ index.js
@@ -0,0 +1,5 @@
+// Write your code in this file!
+let currentUser = "Abdulmajeed";
+let welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+let shortGreeting = `Welcome, ${currentUser[0]}!`;
+let excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`;
